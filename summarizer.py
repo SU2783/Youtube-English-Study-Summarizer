c400469f0f04a2a13906cd66b741d134a81b2501
SU2783 <dbsq280@gmail.com>
2025-03-01 20:27:22 +0900

    Feat: Update gemini model 2.0

--- summarizer.py
+++ summarizer.py
@@ -19,13 +19,13 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate contents from YouTube playlist')
     parser.add_argument('--url', type=str, help='YouTube Video or Playlist URL')
-    parser.add_argument('--model_name', type=str, default='gemini-2.0-pro-exp', help='Generative model name')
+    parser.add_argument('--model_name', type=str, default='gemini-2.0-flash', help='Generative model name')
     return parser.parse_args()
 
 
 def main(
     youtube_url: str,
-    model_name: str = 'gemini-2.0-pro-exp',
+    model_name: str = 'gemini-2.0-flash',
 ):
     # Set up generative model config
     generation_config = genai.GenerationConfig(
